eef2ab79153ddff112a4d2e13832029bde9a6317
playcrab@localhost.localdomain <playcrab@localhost.localdomain>
2016-05-26 21:57:56 +0800

    update

--- buy.js
+++ buy.js
@@ -3,6 +3,7 @@ var http = require('http');
 var util = require('util');
 var fs= require('fs');
 var cheerio = require('cheerio');
+var zlib = require('zlib');
 
 if (process.argv.length != 3) {
     console.log("Usage: node flushUser.js hlx.json\n");
@@ -16,34 +17,33 @@ setTimeout(function() {
 	buyPage(function(res)
 	{
 		$ = cheerio.load(res);
-		var tmpFormData = $('form','.tenderForm').serializeArray();
+		var tmpFormData = $('.tenderForm').serializeArray();
 		var formData = {};
 		for (var key in tmpFormData)
 		{
 			formData[tmpFormData[key].name]=tmpFormData[key].value;
 		}
 		
-		userInfo.formData = formData.availableBalance;
-	
+		userInfo.formData = formData;
 		// 投标
 		buy(function(resData){
 			util.log(resData);
 		});
 	});
-}, 1000);
+}, 1);
 
 function buy(callback) {
 	var method = 'POST';
 	var path = '/product/trade/buy';
 	var postData = userInfo.formData;
-	postData.buyAmount = userInfo.buyAmount;
+  postData.buyAmount = userInfo.buyAmount;
 	util.log('buy Data:'+util.inspect(postData));
-	//request(method, path, postData, callback);
+	//request(method, path, postData, function(res){console.log("buy Response:"+res);});
 }
 
 function buyPage(callback) {
 	var method = 'GET';
-	var path = '/product/planning/detail/403586';
+	var path = '/product/planning/detail/'+userInfo.productId;
 	var postData = {};
 	request(method, path, postData, callback);
 }
@@ -54,8 +54,17 @@ function request(method, path, postData, callback) {
 			port : 80,
 			path : path,
 			method : method,
-			headers : {}
+			headers : {
+      }
 		};
+		options.headers['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8';
+		options.headers['Accept-Encoding'] = 'gzip, deflate, sdch';
+		options.headers['Accept-Language'] = 'zh-CN,zh;q=0.8,en;q=0.6,ja;q=0.4';
+		options.headers['Cache-Control'] = 'max-age=0';
+		options.headers['Connection'] = 'keep-alive';
+		options.headers['Host'] = 'www.xiaoniu88.com';
+		options.headers['Upgrade-Insecure-Requests'] = '1';
+		options.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36';
 	
 	if(method == 'POST')
 	{
@@ -80,24 +89,35 @@ function request(method, path, postData, callback) {
 		//console.log('STATUS: ' + res.statusCode);
 		//console.log('HEADERS: ' + JSON.stringify(res.headers));
 
-		res.setEncoding('utf8');
+//		res.setEncoding('utf8');
 		var resData = '';
 		var len = 0;
+    var chunks = [];
 
 		res.on('data', function(chunk) {
-			resData = resData + chunk;
-
+        chunks.push(chunk);
+        len += chunk.length;
 		});
 		res.on('end', function() {
-			//console.log('HEADERS: ' + JSON.stringify(res.headers));
-
 			if (res.headers['set-cookie'] != undefined) {
 				var cookieinfo = res.headers['set-cookie'][0].split(";");
 				userInfo.cookie = cookieinfo[0];
-				
 			}
-			//util.log('response:' + resData);
-			callback(resData);
+
+	util.log('buy Data:'+util.inspect(chunks));
+      var buffer = Buffer.concat(chunks,len);
+      var encoding = res.headers['content-encoding'];
+      if (encoding == 'gzip') {
+        zlib.gunzip(buffer, function(err, decoded) {
+          callback(decoded && decoded.toString());
+        });
+      } else if (encoding == 'deflate') {
+        zlib.inflate(buffer, function(err, decoded) {
+          callback(decoded && decoded.toString());
+        })
+      } else {
+        callback(buffer.toString());
+      }
 		});
 	});
 
